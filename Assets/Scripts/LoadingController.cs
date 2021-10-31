56a643a5720dbd254364f71a57c023c3853f8821
ilhamcahya19 <ilhamcahyaaldaffa@gmail.com>
2021-10-31 18:20:15 +0700

    first commit

---
+++ Assets/Scripts/LoadingController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingController : MonoBehaviour
+
+{
+    private void Start ()
+
+    {
+
+        UserDataManager.Load ();
+
+        SceneManager.LoadScene (1);
+
+    }
+
+}
